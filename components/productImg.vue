0d83b6acac62b0ad1e60640f6e580251f7beed97
Chung-Ying <ag.cyho@gmail.com>
2024-03-26 23:07:20 +0800

    modify: modify photo

--- components/productImg.vue
+++ components/productImg.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { useProductStore } from '@/stores/productInfo'
+// @ts-ignore
+import mangoNoBgImg from '@/assets/images/products/mango-no-bg.png'
+// @ts-ignore
+import mangoDryNoBgImg from '@/assets/images/products/mango-dry-no-bg.png'
+
 const useProduct = useProductStore()
 const { currentIndex } = storeToRefs(useProduct)
 
@@ -9,22 +14,22 @@ const entering = ref<boolean>(false)
 
 function changeImage(index: number) {
   if (index !== currentImgIndex.value) {
-    leaving.value = true
+    // leaving.value = true
     currentImgIndex.value = index
-    setTimeout(() => {
-      entering.value = true
-    }, 500) // 動畫持續時間為 0.5 秒
+    // setTimeout(() => {
+    //   entering.value = true
+    // }, 500) // 動畫持續時間為 0.5 秒
   }
 }
 
-function handleTransitionEnd() {
-  if (leaving.value) {
-    leaving.value = false
-  }
-  if (entering.value) {
-    entering.value = false
-  }
-}
+// function handleTransitionEnd() {
+//   if (leaving.value) {
+//     leaving.value = false
+//   }
+//   if (entering.value) {
+//     entering.value = false
+//   }
+// }
 
 watch(currentImgIndex, (newIndex) => {
   currentIndex.value = newIndex
@@ -33,13 +38,24 @@ watch(currentImgIndex, (newIndex) => {
 
 <template>
   <div class="product-img-container relative h-full w-full">
-    <img
-      :src="useProduct.fruitProducts[currentImgIndex].imgNoBg"
-      alt="product-img"
-      class="product-img absolute left-0 top-0"
-      :class="{ leaving: leaving, entering: entering }"
-      @transitionend="handleTransitionEnd"
-    />
+    <!-- v-if 圖片切換 -->
+    <Transition>
+      <img
+        v-if="currentImgIndex === 0"
+        :src="mangoNoBgImg"
+        alt="product-img"
+        class="product-img absolute left-0 top-0"
+      />
+    </Transition>
+    <Transition>
+      <img
+        v-if="currentImgIndex === 1"
+        :src="mangoDryNoBgImg"
+        alt="product-img"
+        class="product-img absolute left-0 top-0"
+      />
+    </Transition>
+
     <div
       class="product-img-btn-group absolute bottom-16 left-0 flex w-full justify-center gap-5"
     >
@@ -65,11 +81,11 @@ watch(currentImgIndex, (newIndex) => {
   width: 100%;
   height: 50vh;
   object-fit: contain;
-  transition: transform 0.5s ease;
-  transform: translateY(0);
+  /* transition: transform 0.5s ease;
+  transform: translateY(0); */
 }
 
-.leaving {
+/* .leaving {
   transform: translateY(-150%);
   opacity: 0.2;
 }
@@ -77,7 +93,7 @@ watch(currentImgIndex, (newIndex) => {
 .entering {
   transform: translateY(0);
   opacity: 1;
-}
+} */
 
 .product-img-btn {
   border-radius: calc(1rem + 0.75rem);
@@ -85,5 +101,15 @@ watch(currentImgIndex, (newIndex) => {
 
 .active {
   background-color: rgba(0, 0, 0, 0.4);
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
